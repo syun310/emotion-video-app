2d64a304a3c839f1bd6955b7234cbee7ac16c463
syun310 <seongyun7270@gmail.com>
2025-06-15 16:40:21 +0900

    Update emotion_video_app.py

--- emotion_video_app.py
+++ emotion_video_app.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
+import streamlit.components.v1 as components
 
+# 감정과 유튜브 영상 ID 매핑
 video_map = {
     "기쁨": "1aY-TS-4mk4",
     "슬픔": "C1EbNnZ4UgI",
@@ -11,11 +13,12 @@ st.set_page_config(page_title="감정 기반 영상 소멸 시스템", layout="c
 st.title("🎞 감정 기반 영상 소멸 시스템")
 st.markdown("지금 느끼는 감정을 아래에 입력해주세요. 해당 감정에 맞는 영상이 재생됩니다.")
 
+# 감정 입력
 user_input = st.text_input("✍ 지금 어떤 감정이 드시나요?", placeholder="예: 기쁨, 슬픔, 분노...")
 
+# 감정 분석
 if user_input:
     emotion = "기타"
-
     if "기쁨" in user_input or "행복" in user_input:
         emotion = "기쁨"
     elif "슬픔" in user_input:
@@ -25,17 +28,16 @@ if user_input:
 
     st.success(f"감지된 감정: {emotion}")
 
+    # iframe으로 유튜브 영상 삽입
     if emotion in video_map:
         video_id = video_map[emotion]
-        st.components.v1.html(
+        youtube_url = f"https://www.youtube.com/embed/{video_id}?autoplay=1&mute=1&rel=0&modestbranding=1"
+        components.html(
             f"""
-            <iframe width="100%" height="400" 
-                src="https://www.youtube.com/embed/{video_id}?rel=0&modestbranding=1&autoplay=1&mute=1" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen>
+            <iframe width="100%" height="400" src="{youtube_url}" 
+                frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
             </iframe>
-            """, 
+            """,
             height=400
         )
     else:
